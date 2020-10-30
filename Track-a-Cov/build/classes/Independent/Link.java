65e03f268494416903dfa0aebf9344e003157395
rishank-barua1 <60109790+rishank-barua1@users.noreply.github.com>
2020-10-30 11:08:09 +0530

    Update Link.java

--- Track-a-Cov/build/classes/Independent/Link.java
+++ Track-a-Cov/build/classes/Independent/Link.java
@@ -1,25 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Independent;
-
-/**
- *
- * @author rishu
- */
 import java.awt.Desktop;
 import java.net.URI;
 public class Link {
     public static void main(String[] args){
         String s = "https://www.who.int/";  
         try{
-        Desktop d = Desktop.getDesktop();
-        d.browse(new URI(s));                 
+        Desktop d = Desktop.getDesktop();  
+        d.browse(new URI(s));              //passing the string as an argument   
     }
         catch(Exception e){
-            System.out.println("An error occured "+e.getMessage());
+            System.out.println("An error occured "+e.getMessage());          //exception handled in case a false URL is entered
         }
     }
     
